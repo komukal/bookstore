00f714b9607924748481f6ef5274cb56a8f615b8
komukal <komukal@hotmail.com>
2021-01-31 19:15:35 +0200

    First commit

---
+++ src/main/java/com/course/bookstore/domain/Book.java
@@ -0,0 +1,52 @@
+package com.course.bookstore.domain;
+
+import java.math.BigDecimal;
+
+public class Book {
+	private String title;
+	private String author;
+	private int year;
+	private String isbn;
+	private BigDecimal price;
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getAuthor() {
+		return author;
+	}
+
+	public void setAuthor(String author) {
+		this.author = author;
+	}
+
+	public int getYear() {
+		return year;
+	}
+
+	public void setYear(int year) {
+		this.year = year;
+	}
+
+	public String getIsbn() {
+		return isbn;
+	}
+
+	public void setIsbn(String isbn) {
+		this.isbn = isbn;
+	}
+
+	public BigDecimal getPrice() {
+		return price;
+	}
+
+	public void setPrice(BigDecimal price) {
+		this.price = price;
+	}
+
+}
