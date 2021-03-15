2d469f94db730c2da9dd0ecb71029d0af1910259
komukal <komukal@hotmail.com>
2021-03-15 20:17:04 +0200

    Securing the web application Part II. Created user entity and crud repo, implemented userdetail service

--- src/main/java/com/course/bookstore/BookstoreApplication.java
+++ src/main/java/com/course/bookstore/BookstoreApplication.java
@@ -9,18 +9,21 @@ import com.course.bookstore.domain.Book;
 import com.course.bookstore.domain.BookRepository;
 import com.course.bookstore.domain.Category;
 import com.course.bookstore.domain.CategoryRepository;
+import com.course.bookstore.domain.User;
+import com.course.bookstore.domain.UserRepository;
 
 @SpringBootApplication
-public class BookstoreApplication { 
+public class BookstoreApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BookstoreApplication.class, args);
 	}
 
 	@Bean
-	public CommandLineRunner demo(BookRepository brepository, CategoryRepository crepository) {
+	public CommandLineRunner demo(BookRepository brepository, CategoryRepository crepository,
+			UserRepository urepository) {
 		return (args) -> {
-			
+
 			Category c1 = new Category("Education");
 			Category c2 = new Category("Autobiography");
 			Category c3 = new Category("Fiction");
@@ -28,14 +31,23 @@ public class BookstoreApplication {
 			crepository.save(c1);
 			crepository.save(c2);
 			crepository.save(c3);
-			
-			Book book1 = new Book("Java Concurrency in Practice", "Brian Goetz", 2006, "978-0321349606", 33.13,crepository.findByName("Education").get(0));
-			Book book2 = new Book("A Promised Land", "Barack Obama", 2020, "978-1524763169", 23.96,crepository.findByName("Autobiography").get(0));
-			Book book3 = new Book("Becoming ","Michelle Obama", 2018, "978-1524763138", 18.99,crepository.findByName("Autobiography").get(0));
-			
+
+			Book book1 = new Book("Java Concurrency in Practice", "Brian Goetz", 2006, "978-0321349606", 33.13,
+					crepository.findByName("Education").get(0));
+			Book book2 = new Book("A Promised Land", "Barack Obama", 2020, "978-1524763169", 23.96,
+					crepository.findByName("Autobiography").get(0));
+			Book book3 = new Book("Becoming ", "Michelle Obama", 2018, "978-1524763138", 18.99,
+					crepository.findByName("Autobiography").get(0));
+
 			brepository.save(book1);
 			brepository.save(book2);
 			brepository.save(book3);
+
+			User user1 = new User("user", "$2a$06$3jYRJrg0ghaaypjZ/.g4SethoeA51ph3UD4kZi9oPkeMTpjKU5uo6", "USER");
+			User user2 = new User("admin", "$2a$10$0MMwY.IQqpsVc1jC8u7IJ.2rT8b0Cd3b3sfIBGV2zfgnPGtT4r0.C", "ADMIN");
+			urepository.save(user1);
+			urepository.save(user2);
+
 		};
 	}
 }
--- src/main/java/com/course/bookstore/web/BookController.java
+++ src/main/java/com/course/bookstore/web/BookController.java
@@ -1,8 +1,5 @@
 package com.course.bookstore.web;
 
-import java.util.List;
-import java.util.Optional;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
@@ -11,8 +8,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.course.bookstore.domain.Book;
 import com.course.bookstore.domain.BookRepository;
