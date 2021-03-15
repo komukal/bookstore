1a6e27083676985ec501f3eb11622ed7be0ab065
komukal <komukal@hotmail.com>
2021-03-15 21:07:18 +0200

    Added simple 'smoke' testing with junit

--- src/test/java/com/course/bookstore/BookstoreApplicationTests.java
+++ src/test/java/com/course/bookstore/BookstoreApplicationTests.java
@@ -1,13 +1,25 @@
 package com.course.bookstore;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import static org.assertj.core.api.Assertions.*;
 
+import com.course.bookstore.web.BookController;
+
+@ExtendWith(SpringExtension.class)
 @SpringBootTest
 class BookstoreApplicationTests {
 
+	@Autowired
+	private BookController controller;
+
 	@Test
-	void contextLoads() {
+	public void contextLoads() throws Exception {
+		assertThat(controller).isNotNull();
+
 	}
 
 }
